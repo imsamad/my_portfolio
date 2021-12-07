6f358853fe258d7bb8b734654dadacf509c085d1
imsamad <asdevlpr1206@gmail.com>
2021-12-07 14:37:28 +0530

    Added Go To Top Btn

---
+++ components/TopBtn/index.js
@@ -0,0 +1,15 @@
+import Fab from "@mui/material/Fab";
+import Box from "@mui/system/Box";
+import ArrowUpwardIcon from "@mui/icons-material/ArrowUpward";
+
+const index = () => {
+  return (
+    <Box sx={{ position: "fixed", bottom: 20, right: 20, zIndex: 100 }}>
+      <Fab color="primary" size="medium" onClick={() => window.scrollTo(0, 0)}>
+        <ArrowUpwardIcon size="medium" />
+      </Fab>
+    </Box>
+  );
+};
+
+export default index;
--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,14 @@
-import * as React from "react";
 import PropTypes from "prop-types";
 import Head from "next/head";
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { CacheProvider } from "@emotion/react";
+
 import theme from "../mui/theme";
 import createEmotionCache from "../mui/createEmotionCache";
+import Box from "@mui/system/Box";
+import TopBtn from "../components/TopBtn";
 
-import { Box } from "@mui/system";
-// import Header from '../components/Header';
-// Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
@@ -18,12 +17,10 @@ export default function MyApp(props) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
-        <title>imsamad</title>
+        <title>Abdus Samad</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
       <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-
         <CssBaseline />
         <Box
           sx={{
@@ -31,8 +28,8 @@ export default function MyApp(props) {
             maxWidth: "100vw",
           }}
         >
-          {/* <Header /> */}
           <Component {...pageProps} />
+          <TopBtn />
           <style>{`
           html{
             scroll-behavior: smooth;
